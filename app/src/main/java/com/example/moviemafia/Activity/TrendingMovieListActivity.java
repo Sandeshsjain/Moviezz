87a5428bd8652502fc9f8d5067e9c3a57d7d74bf
Sandeshsjain <sandesh.jain.7374@gmail.com>
2020-11-06 19:00:25 +0530

    final commit

---
+++ app/src/main/java/com/example/moviemafia/Activity/TrendingMovieListActivity.java
@@ -0,0 +1,14 @@
+package com.example.moviemafia.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrendingMovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trending_movie_list);
+    }
+}
